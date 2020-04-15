0d745d1f00cf6c2d39bbbc05be08118747421dfe
matthew.koziel@tmaxsoft.com <matthew.koziel@tmaxsoft.com>
2020-04-15 10:15:51 -0400

    #RM2795-7 First Draft Setup.py
    Used os.environ.get for OpenFrame home variable.
    For testing, set OPENFRAME_HOME in my vm to
    ~/GitLab
    Created listing folder cob_listings for all COBOL Listing files
    Created string suffix for date and time for listings
    Created temps directory and logs directory inside workdir
    Change move source code to copy source code
    Use shutil.copy instead of move.

--- oftools_compile/SetupJob.py
+++ oftools_compile/SetupJob.py
@@ -1,17 +1,34 @@
+import os
+import shutil
+from datetime import datetime
+
 from .Job import Job
 
 
 class SetupJob(Job):
+    base_dir = os.environ.get("OPENFRAME_HOME")
+    listing_dir = os.path.join(base_dir, "cob_listings")
 
     def __init__(self):
+        self.now = datetime.now()
+        self.nowsuffix = self.now.strftime("_%Y%m%d_%H%M%S")
         return
 
     def run(self, in_file):
         print('run SetupJob')
 
         # create_workdir
-        # move source to workdir
+        time_workdir = in_file.rsplit('/', 1)[1] + self.nowsuffix
+        workdir = os.path.join(self.listing_dir, time_workdir)
+        os.mkdir(workdir)
+        tempdir = os.path.join(workdir, "temps")
+        os.mkdir(tempdir)
+        logdir = os.path.join(workdir, "logs")
+        os.mkdir(logdir)
+        # copy source to workdir
+        shutil.copy(in_file, tempdir)
         # change directory to workdir
+        os.chdir(str(workdir))
 
         out_file = in_file.rsplit('/', 1)[1]
 
